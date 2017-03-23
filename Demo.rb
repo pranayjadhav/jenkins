37b08483a5f07e34bfe2758bd8492810e7c0c308
Pranay Jadhav <pranay.jadhav@enzigma.in>
2017-03-24 05:05:40 +0530

    ---commit changes

--- Demo.rb
+++ Demo.rb
@@ -1,9 +1,11 @@
 require 'fileutils'
-FileUtils.rmtree %w(auto-packaging force = false) 
+FileUtils.rmtree %w(auto-packaging) 
 Dir.mkdir 'auto-packaging'
 Dir.mkdir 'auto-packaging\src'
 Dir.mkdir 'auto-packaging\src\staticresources'
 
 FileUtils.copy_entry 'src/', 'auto-packaging/src'
 FileUtils.cp 'auto-package.xml', 'auto-packaging\src\package.xml'
+FileUtils.rm Dir.glob('auto-packaging/src/objects/*.object')
+FileUtils.rm 'NotExistFile', :force => true
 puts "Auto packaging completed..."
