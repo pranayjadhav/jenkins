8677a4f498388a7c1986135cc5da3d60c656ac37
Pranay Jadhav <pranay.jadhav@enzigma.in>
2017-03-24 00:07:24 +0530

    --commit changes for auto packaging

--- Demo.rb
+++ Demo.rb
@@ -1,1 +1,9 @@
-puts "Hello, Gits ...!";
+require 'fileutils'
+FileUtils.rmtree %w(auto-packaging)
+Dir.mkdir 'auto-packaging'
+Dir.mkdir 'auto-packaging\src'
+Dir.mkdir 'auto-packaging\src\staticresources'
+
+FileUtils.copy_entry 'src/', 'auto-packaging/src'
+FileUtils.cp 'auto-package.xml', 'auto-packaging\src\package.xml'
+puts "Auto packaging completed.."
