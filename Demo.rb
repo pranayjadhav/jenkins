c4e05ac810cf3d63ce5d081446956eae8e3a7ac7
Pranay Jadhav <pranay.jadhav@enzigma.in>
2017-03-24 21:16:18 +0530

    --commit for remove changes

--- Demo.rb
+++ Demo.rb
@@ -6,6 +6,7 @@ Dir.mkdir 'auto-packaging\src\staticresources'
 
 FileUtils.copy_entry 'src/', 'auto-packaging/src'
 FileUtils.cp 'auto-package.xml', 'auto-packaging\src\package.xml'
-FileUtils.rm Dir.glob('auto-packaging/src/objects/*.object')
+FileUtils.rm Dir.glob('auto-packaging/src/objects/*.*')
+FileUtils.rm Dir.glob('auto-packaging/src/workflows/*.*')
 FileUtils.rm 'NotExistFile', :force => true
 puts "Auto packaging completed..."
